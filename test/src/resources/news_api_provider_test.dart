5a7dea4d05e7ca04cdf1e8fbcf010e7ac8ce26f7
DAsm <zh.daur@gmail.com>
2021-02-20 13:49:06 +0600

    need 2 move proj

---
+++ test/src/resources/news_api_provider_test.dart
@@ -0,0 +1,30 @@
+import 'package:news/src/resources/news_api_proveder.dart';
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+void main() {
+  test('FetchTopIDs', () async {
+    // setup case
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
+
+    final ids = await newsApi.fetchTopIds();
+
+    expect(ids, [1, 2, 3, 4]);
+  });
+
+  test('fetchItems', () async {
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+
+    final item = await newsApi.fetchItem(999);
+    expect(item.id, 123);
+  });
+}
